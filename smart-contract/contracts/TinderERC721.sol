8d36a7803ac0257f63fa7f7f4ce76297a067bbc3
faisalpiyarji <piyarjifaisal@gmail.com>
2022-03-26 18:38:39 +0530

    new changes
    26/03/2022

--- smart-contract/contracts/TinderERC721.sol
+++ smart-contract/contracts/TinderERC721.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
